cd541616e82f8cd0f29fcc90c9f76b01c7d53231
Abhijeet Singh <contact.abhijeetsingh@gmail.com>
2021-05-04 20:10:43 +0530

    Add sound (#6)

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,24 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "gray.50",
+      },
+    },
+  },
+});
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </QueryClientProvider>
